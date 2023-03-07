34292fc8c13b676631dd517d6bace823c2cd54ff
Ilya Shevchenko <Sevc_ph43@student.itstep.org>
2023-03-07 16:02:28 +0200

    update route authorization

--- src/Authorization.js
+++ src/Authorization.js
@@ -10,7 +10,7 @@ function Authorization(){
             <button onClick={() => {
                 axios({
                     method: 'post',
-                    url: 'https:/marketadminreactapp.azurewebsites.net/api/Authentication/Login',
+                    url: 'https://marketuser.azurewebsites.net/api/Authentication/Login',
                     data: {
                         "userName": document.getElementById('login').value,
                         "password": document.getElementById('pass').value
